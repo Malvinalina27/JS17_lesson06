606d750879640df734b774c0e665ce26ac62a087
Malvinalina27 <rasberry-07@mail.ru>
2021-02-09 12:58:30 +0300

    исправила условие введи число

--- js/script.js
+++ js/script.js
@@ -9,25 +9,25 @@ let startGame = function() {
   let randomNumber = Math.floor(Math.random() * 100) + 1;
   console.log(randomNumber);
 
-  let userNumber = +prompt('Угадай число от 1 до 100');
+  let userNumber = prompt('Угадай число от 1 до 100');
   
   let searchNumber = function() {
       
     if (userNumber > randomNumber) {
       alert('Загаданное число меньше');
-      userNumber = +prompt('Угадай число от 1 до 100');
+      userNumber = prompt('Угадай число от 1 до 100');
       searchNumber();
-    } else if (userNumber < randomNumber && userNumber > 0) {
+    } else if (+userNumber < randomNumber && userNumber > 0) {
       alert('Загаданное число больше');
-      userNumber = +prompt('Угадай число от 1 до 100');
+      userNumber = prompt('Угадай число от 1 до 100');
       searchNumber();
-    } else if (!isNumber(userNumber)) {
+    } else if (isNaN(userNumber) || userNumber.trim() === '') {
       alert('Введи число!');
-      userNumber = +prompt('Угадай число от 1 до 100');
+      userNumber = prompt('Угадай число от 1 до 100');
       searchNumber();
-    } else if (userNumber === randomNumber) {
+    } else if (+userNumber === randomNumber) {
       alert('Поздравляю, Вы угадали!!!');
-    } else if (userNumber === null || userNumber === 0 || userNumber === '') {
+    } else if (+userNumber === null) {
       alert('Игра окончена');    
     }
   };
