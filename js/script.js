3e45ccaec861087254a5cd9d6c6b2cdbff755dbc
Malvinalina27 <rasberry-07@mail.ru>
2021-02-10 12:45:08 +0300

    Добавила isNumber

--- js/script.js
+++ js/script.js
@@ -1,28 +1,27 @@
 'use strict';
 
 let startGame = function() {
-
+  const isNumber = function(n) {
+    return !isNaN(parseFloat(n) && isFinite(n));
+  };
   
   let randomNumber = Math.floor(Math.random() * 100) + 1;
   console.log(randomNumber);
 
-  let userNumber = prompt('Угадай число от 1 до 100');
   
   let searchNumber = function() {
+    let userNumber = prompt('Угадай число от 1 до 100');
       
     if (userNumber === null) {
       alert('Игра окончена');    
+    } else if (isNaN(userNumber) || !isNumber(userNumber)) {
+      alert('Введи число!');
+      searchNumber();
     } else if (userNumber > randomNumber) {
       alert('Загаданное число меньше');
-      userNumber = prompt('Угадай число от 1 до 100');
       searchNumber();
     } else if (userNumber < randomNumber ) {
       alert('Загаданное число больше');
-      userNumber = prompt('Угадай число от 1 до 100');
-      searchNumber();
-    } else if (userNumber === '' || isNaN(userNumber)) {
-      alert('Введи число!');
-      userNumber = prompt('Угадай число от 1 до 100');
       searchNumber();
     } else if (+userNumber === randomNumber) {
       alert('Поздравляю, Вы угадали!!!');
